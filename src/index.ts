6cbed41d19baad2d3e28900daf74177da8d1ddb3
ThomasFlorelli <perso.thomas.florelli@gmail.com>
2022-11-25 16:40:51 +0100

    update: export theme types

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,9 @@ export {
 export type { DropdownOption } from './common-elements';
 export type { OpenAPIEncoding } from './types';
 export * from './services';
-export * from "./types";
+export * from './types';
 export * from './utils';
+export * from './theme';
 
 export * from './styled-components';
 export { default as styled } from './styled-components';
